cbc5c1c005154b5738c2da96f024956228495b7d
David Herbert <daviehert@gmail.com>
2021-10-31 19:36:55 +0100

    Added routes

---
+++ routes/contacts.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// @route    GET api/contacts
+// @desc     Get all user's contact
+// @access   private
+router.get("/", (req, res) => res.send("Get all user's contacts"));
+
+// @route    POST api/contacts
+// @desc     Add new contact
+// @access   private
+router.post("/", (req, res) => res.send("Add new contact"));
+
+// @route    PUT api/contacts:id
+// @desc     Edit a contact
+// @access   private
+router.put("/:id", (req, res) => res.send("Edit contact"));
+
+// @route    DELETE api/contacts/:id
+// @desc     Delete a contact
+// @access   private
+router.delete("/id", (req, res) => res.send("Delete contact"));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 const express = require("express");
 const app = express();
 
-// Create a port or use production port if available
-const PORT = process.env.PORT || 5000;
-
 // Route
 app.get("/", (req, res) =>
   res.json({
@@ -13,5 +10,12 @@ app.get("/", (req, res) =>
     department: "Computer Science",
   })
 );
+// Defining our routes
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("./api/contacts", require("./routes/contacts"));
+app.use("./api/auth", require("./routes/auth"));
+
+// Create a port or use production port if available
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
